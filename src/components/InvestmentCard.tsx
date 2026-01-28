7cab3418e868dd0bc14b709a414084bc6f638258
liajanchira-code <liajanchira@gmail.com>
2026-01-29 03:20:55 +0600

    Add newline at end of InvestmentCard.tsx
    Fix missing newline at end of file.

--- src/components/InvestmentCard.tsx
+++ src/components/InvestmentCard.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { Timer, CheckCircle2, Zap, TrendingUp, Calendar, ArrowUp } from 'lucide-react';
 import { Investment } from '../types';
